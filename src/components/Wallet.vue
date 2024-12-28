dbc2afe4eada98e463886318cfe244cfd6c9d1cf
nknighta <n.knight.pc0627@gmail.com>
2024-12-28 15:00:35 +0900

    feat: 🎸 first commit

---
+++ src/components/Wallet.vue
@@ -0,0 +1,20 @@
+<script>
+</script>
+<template>
+    <div>
+        <h1>Welcome !</h1>
+        <p>Balance: {{ balance }}</p>
+        <button @click="deposit">Deposit</button>
+        <button @click="withdraw">Withdraw</button>
+    </div>
+</template>
+<style scoped>
+    button {
+        margin: 0 0.5rem;
+        color: #bcabff;
+        background-color: #000021;
+        border: none;
+        border-radius: 0.25rem;
+        padding: 0.5rem 1rem;
+    }
+</style>
